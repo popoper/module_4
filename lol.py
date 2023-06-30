dab779f63d4d2c8b41294bb528f05670b5560457
Goose <74737821+popoper@users.noreply.github.com>
2023-06-30 19:26:06 +0300

    Update lol.py
    .

--- lol.py
+++ lol.py
@@ -1,3 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Доска объявлений</title>
+    <link rel="stylesheet" href="styles.css">
+  <style>
+   .stroke {
+    font: bold 3em Arial, sans-serif;
+    color: #0d3967;
+    text-shadow: #008000 1px 1px 0, #008000 2px 2px 0,
+                 #008000 3px 3px 0, #008000 4px 4px 0,
+                 #008000 5px 5px 0;
+   }
+  </style>
+</head>
+<body>
+<h1>Заголовок 1 уровня</h1>
+<h2>Заголовок 2 уровня</h2>
+<h3>Заголовок 3 уровня</h3>
+<h4>Заголовок 4 уровня</h4>
+<h5>Заголовок 5 уровня</h5>
+<h6>Заголовок 6 уровня</h6>
+
+<p class="paragraph-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur cum dolore facilis fugiat id illum iste laboriosam, magnam nisi nostrum, obcaecati praesentium provident quis quod reiciendis rerum soluta sunt totam.</p>
+<p class="paragraph-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur cum dolore facilis fugiat id illum iste laboriosam, magnam nisi nostrum, obcaecati praesentium provident quis quod reiciendis rerum soluta sunt totam.</p>
+<p class="paragraph-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur cum dolore facilis fugiat id illum iste laboriosam, magnam nisi nostrum, obcaecati praesentium provident quis quod reiciendis rerum soluta sunt totam.</p>
+
+<font size="24">
+    <p size="24" class="paragraph-4"><i><center>Конструктор сайтов "Нубекс"</center></i>
+</font>
+
+
+<div>
+<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis optio porro, praesentium quia repudiandae soluta totam voluptas? Culpa distinctio error iusto minus officiis quo recusandae velit. Deserunt esse impedit soluta.</span>
+</div>
+
+<ul>
+    <li>Первый элемент</li>
+    <li>Второй элемент</li>
+    <li>Третий элемент</li>
+</ul>
+
+<ol>
+    <li>Первый элемент</li>
+    <li>Второй элемент</li>
+    <li>Третий элемент</li>
+</ol>
+
 <img class="fit-picture"
      src="C:\Users\lunko\PycharmProjects\pythonProject\skelet.gif"
      alt="Spooky, scary skeletons  Send shivers down your spine">
+
+</body>
+</html>
